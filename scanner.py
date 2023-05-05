608f1d5dca0287bd6cc96c8d4dcdf12e638d3311
Jason O'Neal <jason.allen.oneal@gmail.com>
2023-05-04 20:04:49 -0400

    optimizations

--- scanner.py
+++ scanner.py
@@ -1,87 +1,87 @@
-#!/usr/bin/env python
-
-import requests, json, time
+import requests
+import time
 from requests.exceptions import HTTPError
 from urllib.parse import urlencode
 from utils import Utils
 
+
 class Scanner:
-	def __init__(self, options):
-		self.options = options
-		self.base = 'https://http-observatory.security.mozilla.org/api/v1/'
-		self.start = time.time()
-		self.end = None
-		self.scanId = None
-		self.state = None
-		self.running = True
-		
-		self.utils = Utils()
-		self.statusResult = None
-		self.scanResult = None
-	
-	def makePost(self, action, params):
-		try:
-			response = requests.post(f'{self.base}{action}?host={self.options.target}', params)
-			response.raise_for_status()
-			if self.options.verbosity:
-				print('Scan initiation response: ', end='')
-				print(response.json())
-			return response.json()
-		except HTTPError as http_err:
-			self.utils.msg(f'HTTP error occurred: {http_err}', 'error')
-			exit(1)
-		except Exception as err:
-			self.utils.msg(f'Other error occurred: {err}', 'error')
-			exit(1)
-	
-	def makeGet(self, action, params):
-		try:
-			p = urlencode(params)
-			response = requests.get(f'{self.base}{action}?{p}')
-			if self.options.verbosity:
-				print(f'{action} response: ', end='')
-				print(response.json())
-			return response.json()
-		except HTTPError as http_err:
-			self.utils.msg(f'HTTP error occurred: {http_err}', 'error')
-			exit(1)
-		except Exception as err:
-			self.utils.msg(f'Other error occurred: {err}', 'error')
-			exit(1)
-	
-	def begin(self):
-		result = self.makePost('analyze', {'hidden': 'true', 'rescan': 'true'})
-		
-		if "error" in result:
-			err = result['text']
-			if "cooldown" in err:
-				if self.running != 'cooldown':
-					self.utils.msg(f'{self.options.target} is in cooldown.  Waiting 3 minutes as required by the API.', 'warn')
-				self.running = 'cooldown'
-				self.utils.countdown(int(3*60), self.rescan)
-			else:
-				self.utils.msg(err, 'error')
-				exit(1)
-		else:
-			self.state = result['state']
-			self.scanId = result['scan_id']
-	
-	def checkResults(self):
-		self.statusResult = self.makeGet('analyze', {'host': self.options.target})
-		
-		if 'error' in self.statusResult:
-			self.utils.msg(self.statusResult['text'], 'error')
-		else:
-			self.state = self.statusResult['state']
-		
-		if self.state == 'FINISHED':
-			self.checkTests()
-	
-	def checkTests(self):
-		self.scanResult = self.makeGet('getScanResults', {'scan': self.scanId})
-		self.running = False
-		self.end =time.time()
-	
-	def rescan(self):
-		self.running = True
-		self.begin()
+    def __init__(self, options):
+        self.options = options
+        self.base = 'https://http-observatory.security.mozilla.org/api/v1/'
+        self.start = time.time()
+        self.end = None
+        self.scan_id = None
+        self.state = None
+        self.running = True
+
+        self.utils = Utils()
+        self.status_result = None
+        self.scan_result = None
+
+    def make_post(self, action, params):
+        try:
+            response = requests.post(f'{self.base}{action}?host={self.options.target}', params)
+            response.raise_for_status()
+            if self.options.verbosity:
+                print('Scan initiation response: ', end='')
+                print(response.json())
+            return response.json()
+        except HTTPError as http_err:
+            self.utils.msg(f'HTTP error occurred: {http_err}', 'error')
+            exit(1)
+        except Exception as err:
+            self.utils.msg(f'Other error occurred: {err}', 'error')
+            exit(1)
+
+    def make_get(self, action, params):
+        try:
+            p = urlencode(params)
+            response = requests.get(f'{self.base}{action}?{p}')
+            if self.options.verbosity:
+                print(f'{action} response: ', end='')
+                print(response.json())
+            return response.json()
+        except HTTPError as http_err:
+            self.utils.msg(f'HTTP error occurred: {http_err}', 'error')
+            exit(1)
+        except Exception as err:
+            self.utils.msg(f'Other error occurred: {err}', 'error')
+            exit(1)
+
+    def begin(self):
+        result = self.make_post('analyze', {'hidden': 'true', 'rescan': 'true'})
+
+        if "error" in result:
+            err = result['text']
+            if "cooldown" in err:
+                if self.running != 'cooldown':
+                    self.utils.msg(f'{self.options.target} is in cooldown. Waiting 3 minutes as required by the API.', 'warn')
+                self.running = 'cooldown'
+                self.utils.countdown(int(3*60), self.rescan)
+            else:
+                self.utils.msg(err, 'error')
+                exit(1)
+        else:
+            self.state = result['state']
+            self.scan_id = result['scan_id']
+
+    def check_results(self):
+        self.status_result = self.make_get('analyze', {'host': self.options.target})
+
+        if 'error' in self.status_result:
+            self.utils.msg(self.status_result['text'], 'error')
+        else:
+            self.state = self.status_result['state']
+
+        if self.state == 'FINISHED':
+            self.check_tests()
+
+    def check_tests(self):
+        self.scan_result = self.make_get('getScanResults', {'scan': self.scan_id})
+        self.running = False
+        self.end = time.time()
+
+    def rescan(self):
+        self.running = True
+        self.begin()
--- utils.py
+++ utils.py
@@ -1,51 +1,52 @@
 #!/usr/bin/env python
 
-import re, time
+import re
+import time
 from sys import platform
 from colorama import Fore, Style
 from rich.console import Console
 from rich.table import Table
 
+
 class Utils:
-	def __init__(self):
-		self.console = Console()
-	
-	def msg(self, text, level):
-		if 'error' in level:
-			print(f'{Fore.RED}[!]{Style.RESET_ALL} '+text)
-		if 'warn' in level:
-			print(f'{Fore.YELLOW}[!]{Style.RESET_ALL} '+text)
-		if 'info' in level:
-			print(f'{Fore.BLUE}[*]{Style.RESET_ALL} '+text)
-		if 'success' in level:
-			print(f'{Fore.GREEN}[$]{Style.RESET_ALL} '+text)
-		if 'title' in level:
-			print(f'{Fore.CYAN}'+text+f'{Style.RESET_ALL}')
-	
-	def countdown(self, t, cb):
-		while t >= 0:
-			mins, secs = divmod(t, 60)
-			timer = '{:02d}:{:02d}'.format(mins, secs)
-			print(f'{Fore.BLUE}{timer}{Style.RESET_ALL}', end="\r")
-			time.sleep(1)
-			t -= 1
-		print(end='')
-		cb()
-	
-	def displayElapsed(self, t):
-		print(f"{Fore.BLUE}{int(t/3600)}H {int((t/60)%60) if t/3600>0 else int(t/60)}M {int(t%60)}S{Style.RESET_ALL}")
-	
-	def printResult(self, data, elapsed):
-		print(f'{Fore.GREEN}Scan completed in: {Style.RESET_ALL}', end='')
-		self.displayElapsed(elapsed)
-		self.msg('Results', 'title')
-		
-		table = Table(show_header=True, header_style="bold blue")
-		table.add_column("Test")
-		table.add_column("Pass")
-		table.add_column("Explanation")
-		
-		for key in data.keys():
-			table.add_row(data[key]['name'], f"{data[key]['pass']}", data[key]['score_description'], end_section=True)
-		
-		self.console.print(table)
+    def __init__(self):
+        self.console = Console()
+
+    def msg(self, text, level):
+        if 'error' in level:
+            print(f'{Fore.RED}[!]{Style.RESET_ALL} {text}')
+        if 'warn' in level:
+            print(f'{Fore.YELLOW}[!]{Style.RESET_ALL} {text}')
+        if 'info' in level:
+            print(f'{Fore.BLUE}[*]{Style.RESET_ALL} {text}')
+        if 'success' in level:
+            print(f'{Fore.GREEN}[$]{Style.RESET_ALL} {text}')
+        if 'title' in level:
+            print(f'{Fore.CYAN}{text}{Style.RESET_ALL}')
+
+    def countdown(self, t, cb):
+        for i in range(t, -1, -1):
+            mins, secs = divmod(i, 60)
+            timer = f'{mins:02d}:{secs:02d}'
+            print(f'{Fore.BLUE}{timer}{Style.RESET_ALL}', end="\r")
+            time.sleep(1)
+        print(end='')
+        cb()
+
+    def displayElapsed(self, t):
+        print(f"{Fore.BLUE}{int(t / 3600)}H {int((t / 60) % 60) if t / 3600 > 0 else int(t / 60)}M {int(t % 60)}S{Style.RESET_ALL}")
+
+    def printResult(self, data, elapsed):
+        print(f'{Fore.GREEN}Scan completed in: {Style.RESET_ALL}', end='')
+        self.displayElapsed(elapsed)
+        self.msg('Results', 'title')
+
+        table = Table(show_header=True, header_style="bold blue")
+        table.add_column("Test")
+        table.add_column("Pass")
+        table.add_column("Explanation")
+
+        for key in data.keys():
+            table.add_row(data[key]['name'], f"{data[key]['pass']}", data[key]['score_description'], end_section=True)
+
+        self.console.print(table)
